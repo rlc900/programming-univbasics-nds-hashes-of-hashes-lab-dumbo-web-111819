a40d6efab613248346f75dc0dc2b791b23e7a585
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-10 23:51:19 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -48,17 +48,6 @@ nested_hash = {
      label: "Phylum",
      sub_category: {
        label: "Class"
-     }
-   },
-   {label: "Order"},
-   {label: "Family",
-   sub_category: {
-     label: "Genus",
-  }
-  },
-{ label: "Species",
-  sub_category: nil
-}
 }
 
 end
